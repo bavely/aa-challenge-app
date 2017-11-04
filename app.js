9a023be6ba62fdaf9cf640951d1d40343fd343da
bavely tawfik <bavlesamy@gmail.com>
2017-11-03 23:33:25 -0700

    Gitnew

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const PORT = 3000 || process.env.PORT;
+const path = require('path');
+
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT} `);
+});
